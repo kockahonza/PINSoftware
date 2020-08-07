09397620aee7502d252883e239591ba548fc1e37
Jan Kočka <kockahonza@gmail.com>
2020-08-07 14:58:29 +0200

    Changed the name and added dependencies

--- setup.py
+++ setup.py
@@ -4,15 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PINDiode-software", # Replace with your own username
-    version="0.0.1",
+    name="PINSoftware", # Replace with your own username
+    version="0.1",
     author="Jan Kocka",
     author_email="kockahonza@gmail.com",
     description="Software for the xPIN + Sample and Hold + NI-6002 setup at ELI Beamlines",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
+    url="https://github.com/kockahonza/PINSoftware",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'dash',
+        'dash-bootstrap-components',
+        'waitress',
+        'nidaqmx',
+        'h5py',
+        'matplotlib'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
