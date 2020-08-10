088dc966585ac0a2c1f5fe7706e76fb79405d4d7
Jan Kočka <kockahonza@gmail.com>
2020-08-10 14:34:20 +0200

    added docstrings and slight reformatting

--- PINSoftware/Debugger.py
+++ PINSoftware/Debugger.py
@@ -2,16 +2,21 @@ import os
 import sys
 
 class Debugger():
+    """
+    A very simple IO handler, it is here to make printouts more consistent and easy to find.
+    Calling `Debugger.error` is also the correct way to exit on error.
+    """
     def __init__(self, exit_on_error=True):
+        """If `exit_on_error` is true, then whenever `Debugger.exit` is called, the program halts."""
         self.exit_on_error = exit_on_error
 
     def info(self, msg):
-        print("Info: " + msg)
+        print("Info:", msg)
 
     def warning(self, msg):
-        print("Warning: " + msg)
+        print("Warning:", msg)
 
-    def error(self, msg):
-        print("Error: " + msg)
+    def error(self, msg, n=1):
+        print("Error:", msg)
         if self.exit_on_error:
-            os._exit(1)
+            os._exit(n)
